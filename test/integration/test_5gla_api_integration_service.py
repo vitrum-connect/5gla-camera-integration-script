caf834d683e76996a2a2f937d4b0d306bf8445a6
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-10-13 12:19:16 +0200

    Add base64 test data for drone image
    This commit introduces a new file: base64_drone_image.txt in the test/integration/data directory. The file contains a base64 representation of a drone image, specifically added for testing purposes. It enhances our suit of tests related to image processing functionalities.

--- test/integration/test_5gla_api_integration_service.py
+++ test/integration/test_5gla_api_integration_service.py
@@ -8,3 +8,19 @@ class ApiIntegrationServiceTest(unittest.TestCase):
     def test_given_running_api_when_checking_availability_then_should_return_true(self):
         api_integration_service = ApiIntegrationService()
         self.assertTrue(api_integration_service.check_availability())
+
+    def test_given_running_api_when_sending_image_then_should_return_true(self):
+        api_integration_service = ApiIntegrationService()
+        self.assertTrue(
+            api_integration_service.send_image(2, 4, 'BLUE', [self._read_base64_encoded_image_as_string_from_file()]))
+
+    def test_given_running_api_when_sending_multiple_images_then_should_return_true(self):
+        api_integration_service = ApiIntegrationService()
+        self.assertTrue(
+            api_integration_service.send_image(2, 4, 'BLUE', [self._read_base64_encoded_image_as_string_from_file(),
+                                                              self._read_base64_encoded_image_as_string_from_file()]))
+
+    @staticmethod
+    def _read_base64_encoded_image_as_string_from_file():
+        with open('./data/base64_drone_image.txt', 'r') as file:
+            return file.read()
