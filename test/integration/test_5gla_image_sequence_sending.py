5eb5c302a259daf6035de972aaba963e58f33625
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-02-20 10:59:14 +0100

    Implement begin and end transaction methods and update config
    The integration service logic was updated to include new begin and end transaction methods, and the config.yaml was also updated to reflect these changes. The transaction ID and drone ID are now passed as parameters to these methods to enhance the flexibility of usage. Furthermore, the file path for base64 image reading in the test case was corrected.

--- test/integration/test_5gla_image_sequence_sending.py
+++ test/integration/test_5gla_image_sequence_sending.py
@@ -11,15 +11,16 @@ class ImageSequenceSendingTest(unittest.TestCase):
         api_integration_service = ApiIntegrationService()
         base64_encoded_images = self._read_all_images_as_base64_encoded_strings_from_files()
         self.assertEqual(5, len(base64_encoded_images))
-        transaction_id = '693197e0-eea3-432c-8cff-a5ee8a7208a2'
-        drone_id = '6f394286-5c51-49ff-ba98-a58de346188f'
+        transaction_id = 'a5ee8a7208a2'
+        drone_id = 'a58de346188f'
+        self.assertTrue(api_integration_service.begin_transaction(drone_id=drone_id, transaction_id=transaction_id))
         for channel, base64_encoded_image in base64_encoded_images.items():
             logging.info(f"Sending image for channel: {channel}")
             self.assertTrue(api_integration_service.send_image(transaction_id=('%s' % transaction_id),
                                                                drone_id=drone_id,
                                                                channel=channel,
                                                                images=[base64_encoded_image]))
-        self.assertTrue(api_integration_service.end_transaction(transaction_id=transaction_id))
+        self.assertTrue(api_integration_service.end_transaction(drone_id=drone_id, transaction_id=transaction_id))
 
     @staticmethod
     def _read_all_images_as_base64_encoded_strings_from_files():
@@ -33,5 +34,5 @@ class ImageSequenceSendingTest(unittest.TestCase):
 
     @staticmethod
     def _read_base64_encoded_image(image_name):
-        with open(f'test/integration/data/encoded_image_set/{image_name}', 'r') as file:
+        with open(f'data/encoded_image_set/{image_name}', 'r') as file:
             return file.read()
