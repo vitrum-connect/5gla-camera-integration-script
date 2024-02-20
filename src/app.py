49418b65827e8b1a64d1121fcdae0e5eb2226d30
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-02-20 13:33:35 +0100

    Add environment variable handler and integration test mode
    A new method 'get_env' has been added to the Config Manager to retrieve environment variables with a default value if the key isn't found. Additionally, added an 'Integration Test' mode in the app, which is enabled by setting the 'INTEGRATION_TEST' environment variable to True. This mode bypasses the API availability check, enhancing testing flexibility.

--- src/app.py
+++ src/app.py
@@ -11,32 +11,33 @@ camera_integration_service = CameraIntegrationService()
 api_integration_service = ApiIntegrationService()
 transaction_id = uuid.uuid4().__str__()
 
-logging.info("Checking the availability of the API.")
-
-if not api_integration_service.check_availability():
-    logging.error("API is not available.")
+if config_manager.get_env('INTEGRATION_TEST', False):
+    logging.info("Integration test mode is enabled.")
 else:
-    logging.info("API is available, starting the process.")
-    if camera_integration_service.drone_has_lifted_off():
-        api_integration_service.begin_transaction(drone_id=config_manager.get('drone_id'),
-                                                  transaction_id=transaction_id)
+    if not api_integration_service.check_availability():
+        logging.error("API is not available.")
+    else:
+        logging.info("API is available, starting the process.")
+        if camera_integration_service.drone_has_lifted_off():
+            api_integration_service.begin_transaction(drone_id=config_manager.get('drone_id'),
+                                                      transaction_id=transaction_id)
 
-        number_of_sets_taken = 0
-        while camera_integration_service.drone_is_flying():
-            logging.info("Triggering camera to take pictures for set number: " + str(number_of_sets_taken))
-            logging.debug("Triggering camera to take pictures.")
-            camera_integration_service.trigger_pictures(config_manager.get('trigger_photo_url'))
-            logging.debug("Downloading picture into the given folder.")
-            folder = config_manager.get_env('WORKING_DIR') + config_manager.get(
-                'photo_folder') + transaction_id + "/" + str(
-                int(time.time())) + '/'
-            camera_integration_service.create_folder(folder)
-            camera_integration_service.download_picture(config_manager.get('photo_download_url'), folder)
-            camera_integration_service.send_pictures_via_api(drone_id=config_manager.get('drone_id'),
-                                                             transaction_id=transaction_id,
-                                                             folder=folder)
-            time.sleep(config_manager.get('photo_interval_seconds'))
-            number_of_sets_taken += 1
+            number_of_sets_taken = 0
+            while camera_integration_service.drone_is_flying():
+                logging.info("Triggering camera to take pictures for set number: " + str(number_of_sets_taken))
+                logging.debug("Triggering camera to take pictures.")
+                camera_integration_service.trigger_pictures(config_manager.get('trigger_photo_url'))
+                logging.debug("Downloading picture into the given folder.")
+                folder = config_manager.get_env('WORKING_DIR') + config_manager.get(
+                    'photo_folder') + transaction_id + "/" + str(
+                    int(time.time())) + '/'
+                camera_integration_service.create_folder(folder)
+                camera_integration_service.download_picture(config_manager.get('photo_download_url'), folder)
+                camera_integration_service.send_pictures_via_api(drone_id=config_manager.get('drone_id'),
+                                                                 transaction_id=transaction_id,
+                                                                 folder=folder)
+                time.sleep(config_manager.get('photo_interval_seconds'))
+                number_of_sets_taken += 1
 
-        api_integration_service.end_transaction(drone_id=config_manager.get('drone_id'),
-                                                transaction_id=transaction_id)
+            api_integration_service.end_transaction(drone_id=config_manager.get('drone_id'),
+                                                    transaction_id=transaction_id)
